fb495dccafaee2cf8fd64fc8bbec67b2d50327d4
Berk Erkul <Berk Erkul>
2016-07-29 22:52:29 -0700

    Add teleport mechanic, upgrade to unity 5.4

---
+++ TeamArcher/Assets/Scripts/TeleportArrow.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TeleportArrow : Arrow {
+
+    public GameObject player = null;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("Teleport arrow collided!");
+        if (player == null)
+        {
+            Debug.Log("Player was null. Setting with tag.");
+            player = GameObject.FindGameObjectWithTag("Player");
+        }
+        var pos = collision.contacts[0].point;
+        Debug.Log("player position: " + player.transform.position + " teleporting to: " + pos);
+        player.transform.position = pos;
+                
+        if (inFlight)
+        {
+            ResetArrow();
+        }
+    }
+}
